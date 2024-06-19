326b464095e679684377e8d7df22434c0ca34e68
RSM-git <rasmus.steen.mikkelsen@hotmail.com>
2024-06-19 15:37:07 +0200

    abstract class for introducing new transitions and more

--- src/SIR.py
+++ src/SIR.py
@@ -1,6 +1,83 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from abc import ABC, abstractmethod
+
+
+class GenericSIR(ABC):
+    def __init__(self, parameters: tuple, states, N: int, t_max: float):
+        self.parameters = parameters
+        self.N = N
+        self.t_max = t_max
+        self.states = states
+
+    def simulate(self):
+        while self.states["T"][-1] < self.t_max and self.states["I"][-1] > 0:
+            rates = self.rates()
+            rate = rates.sum()
+            time = np.random.exponential(1/rate)
+
+            self.states["T"].append(self.states["T"][-1] + time)
+
+            event = np.random.choice(range(len(rates)), p=rates/rate)
+
+            self.event_callback(event)
+
+
+    @abstractmethod
+    def rates(self):
+        raise NotImplementedError("TODO: Missing 1 line")
+
+    @abstractmethod
+    def event_callback(self, event):
+        raise NotImplementedError("TODO: Missing 1 line")
+
+
+class CTMC_SIR3(GenericSIR):
+    def rates(self):
+        S, I = self.states["S"][-1], self.states["I"][-1]
+        beta, gamma = self.parameters
+
+        return np.array([beta*S*I/self.N, gamma*I])
+    
+    def event_callback(self, event):
+        if event == 0:
+            self.states["S"].append(self.states["S"][-1]-1)
+            self.states["I"].append(self.states["I"][-1]+1)
+            self.states["R"].append(self.states["R"][-1])
+        else:
+            self.states["S"].append(self.states["S"][-1])
+            self.states["I"].append(self.states["I"][-1]-1)
+            self.states["R"].append(self.states["R"][-1]+1)
+
+
+class CTMC_SIR_Death(GenericSIR):
+    def rates(self):
+        S, I = self.states["S"][-1], self.states["I"][-1]
+        beta, gamma, mu = self.parameters
+
+        return np.array([beta*S*I/self.N, gamma*I, mu*I])
+    
+    def event_callback(self, event):
+        if event == 0:
+            self.states["S"].append(self.states["S"][-1]-1)
+            self.states["I"].append(self.states["I"][-1]+1)
+            self.states["R"].append(self.states["R"][-1])
+            self.states["D"].append(self.states["D"][-1])
+        elif event == 1:
+            self.states["S"].append(self.states["S"][-1])
+            self.states["I"].append(self.states["I"][-1]-1)
+            self.states["R"].append(self.states["R"][-1]+1)
+            self.states["D"].append(self.states["D"][-1])
+        elif event == 2:
+            self.states["I"].append(self.states["I"][-1]-1)
+            self.states["D"].append(self.states["D"][-1]+1)
+            self.states["S"].append(self.states["S"][-1])
+            self.states["R"].append(self.states["R"][-1])
+        
+
+
+
 class CTMC_SIR:
     def __init__(self, beta: float, gamma: float, N: int, I0: int, t_max: float):
         self.beta = beta
@@ -36,19 +113,39 @@ class CTMC_SIR:
 
 if __name__ == "__main__":
     ebola = (0.2, 0.1)
+    covid = (0.17, 0.082)
     flu = (1.37383, 0.98622)
     swine_flu = (7/15, 1/3)
-    rasmus = (0.1, 1/30)
-    N = 10_000
-    t_max = 2000
-    I0 = 2000
+    custom_disease = (0.45, 1/30)
+    N = 50_000
+    t_max = 1000
+    I0 = 100
 
+    extended_covid = (0.17, 0.082*9/10, 0.0082)
+
+    states = {"S": [N - I0],
+              "I": [I0],
+              "R": [0],
+              "T": [0],
+              "D": [0]
+                       }
     # SIR = CTMC_SIR(*flu, N, I0, t_max)
-    SIR = CTMC_SIR(*rasmus, N, I0, t_max)
-    S, I, R, T = SIR.simulate()
+    SIR = CTMC_SIR_Death(extended_covid, states, N, t_max)
+
+    SIR.simulate()
+    S = SIR.states["S"]
+    I = SIR.states["I"]
+    R = SIR.states["R"]
+    T = SIR.states["T"]
+    D = SIR.states["D"]
+
+    print(S[-1], I[-1], R[-1])
     plt.plot(T, I)
     plt.plot(T, S)
     plt.plot(T, R)
+    plt.plot(T, D)
+    plt.legend(["I", "S", "R", "Dead:)"])
+    plt.xlabel("Days")
     plt.show()
 
 
